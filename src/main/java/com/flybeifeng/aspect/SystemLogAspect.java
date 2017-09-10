63a69a3528b37a3c9a1afb097115e66f2a104ea3
flybeifeng <flybeifeng@gmail.com>
2017-09-10 14:38:33 +0800

    添加aop demo

---
+++ src/main/java/com/flybeifeng/aspect/SystemLogAspect.java
@@ -0,0 +1,201 @@
+package com.flybeifeng.aspect;
+
+import com.flybeifeng.annotation.SystemControllerLog;
+import com.flybeifeng.annotation.SystemServiceLog;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.lang.reflect.Method;
+import java.util.Enumeration;
+
+@Aspect
+@Component
+public class SystemLogAspect {
+
+    //Service层切点
+    @Pointcut("@annotation(com.flybeifeng.annotation.SystemServiceLog)")
+    public void serviceAspect() {
+    }
+
+    //Controller层切点
+    @Pointcut("@annotation(com.flybeifeng.annotation.SystemControllerLog)")
+    public void controllerAspect() {
+    }
+
+    /**
+     * 前置通知 用于拦截Controller层记录用户的操作
+     *
+     * @param joinPoint 切点
+     */
+    @Before("controllerAspect()")
+    public void doBefore(JoinPoint joinPoint) {
+
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+        HttpSession session = request.getSession();
+        Enumeration<String> attributeNames = session.getAttributeNames();
+        while (attributeNames.hasMoreElements()) {
+            String s = attributeNames.nextElement();
+            Object attribute = session.getAttribute(s);
+            System.out.println(s + "--->" + attribute);
+        }
+        //读取session中的用户
+//        User user = (User) session.getAttribute(WebConstants.CURRENT_USER);
+        //请求的IP
+        String ip = request.getRemoteAddr();
+        try {
+            //*========控制台输出=========*//
+            System.out.println("=====前置通知开始=====");
+            System.out.println("请求方法:" + (joinPoint.getTarget().getClass().getName() + "." + joinPoint.getSignature().getName() + "()"));
+            System.out.println("方法描述:" + getControllerMethodDescription(joinPoint));
+//            System.out.println("请求人:" + user.getName());
+            System.out.println("请求IP:" + ip);
+            //*========数据库日志=========*//
+
+            System.out.println("=====前置通知结束=====");
+        } catch (Exception e) {
+            //记录本地异常日志
+            System.out.println("==前置通知异常==");
+            System.out.println("异常信息:{}" + e.getMessage());
+        }
+    }
+
+    /**
+     * 前置通知 用于拦截Controller层记录用户的操作
+     *
+     * @param joinPoint 切点
+     */
+    @After("controllerAspect()")
+    public void doAfter(JoinPoint joinPoint) {
+
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+        HttpSession session = request.getSession();
+        Enumeration<String> attributeNames = session.getAttributeNames();
+        while (attributeNames.hasMoreElements()) {
+            String s = attributeNames.nextElement();
+            Object attribute = session.getAttribute(s);
+            System.out.println(s + "--->" + attribute);
+        }
+        //读取session中的用户
+//        User user = (User) session.getAttribute(WebConstants.CURRENT_USER);
+        //请求的IP
+        String ip = request.getRemoteAddr();
+        try {
+            //*========控制台输出=========*//
+            System.out.println("====后置通知开始=====");
+            System.out.println("请求方法:" + (joinPoint.getTarget().getClass().getName() + "." + joinPoint.getSignature().getName() + "()"));
+            System.out.println("方法描述:" + getControllerMethodDescription(joinPoint));
+//            System.out.println("请求人:" + user.getName());
+            System.out.println("请求IP:" + ip);
+            //*========数据库日志=========*//
+
+            System.out.println("=====后置通知结束=====");
+        } catch (Exception e) {
+            //记录本地异常日志
+            System.out.println("==后置通知异常==");
+            System.out.println("异常信息:{}" + e.getMessage());
+        }
+    }
+
+
+    /**
+     * 异常通知 用于拦截service层记录异常日志
+     *
+     * @param joinPoint
+     * @param e
+     */
+    @AfterThrowing(pointcut = "serviceAspect()", throwing = "e")
+    public void doAfterThrowing(JoinPoint joinPoint, Throwable e) {
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+        HttpSession session = request.getSession();
+        //读取session中的用户
+//        User user = (User) session.getAttribute(WebConstants.CURRENT_USER);
+        //获取请求ip
+        String ip = request.getRemoteAddr();
+        //获取用户请求方法的参数并序列化为JSON格式字符串
+        String params = "";
+        if (joinPoint.getArgs() != null && joinPoint.getArgs().length > 0) {
+            for (int i = 0; i < joinPoint.getArgs().length; i++) {
+                params += joinPoint.getArgs()[i] + ";";
+            }
+        }
+        try {
+              /*========控制台输出=========*/
+            System.out.println("=====异常通知开始=====");
+            System.out.println("异常代码:" + e.getClass().getName());
+            System.out.println("异常信息:" + e.getMessage());
+            System.out.println("异常方法:" + (joinPoint.getTarget().getClass().getName() + "." + joinPoint.getSignature().getName() + "()"));
+            System.out.println("方法描述:" + getServiceMethodDescription(joinPoint));
+//            System.out.println("请求人:" + user.getName());
+            System.out.println("请求IP:" + ip);
+            System.out.println("请求参数:" + params);
+               /*==========数据库日志=========*/
+
+            System.out.println("=====异常通知结束=====");
+        } catch (Exception ex) {
+            //记录本地异常日志
+            System.out.println("==异常通知异常==");
+            System.out.println("异常信息:{}" + ex.getMessage());
+        }
+         /*==========记录本地异常日志==========*/
+
+    }
+
+
+    /**
+     * 获取注解中对方法的描述信息 用于service层注解
+     *
+     * @param joinPoint 切点
+     * @return 方法描述
+     * @throws Exception
+     */
+    public static String getServiceMethodDescription(JoinPoint joinPoint)
+            throws Exception {
+        String targetName = joinPoint.getTarget().getClass().getName();
+        String methodName = joinPoint.getSignature().getName();
+        Object[] arguments = joinPoint.getArgs();
+        Class targetClass = Class.forName(targetName);
+        Method[] methods = targetClass.getMethods();
+        String description = "";
+        for (Method method : methods) {
+            if (method.getName().equals(methodName)) {
+                Class[] clazzs = method.getParameterTypes();
+                if (clazzs.length == arguments.length) {
+                    description = method.getAnnotation(SystemServiceLog.class).description();
+                    break;
+                }
+            }
+        }
+        return description;
+    }
+
+    /**
+     * 获取注解中对方法的描述信息 用于Controller层注解
+     *
+     * @param joinPoint 切点
+     * @return 方法描述
+     * @throws Exception
+     */
+    public static String getControllerMethodDescription(JoinPoint joinPoint) throws Exception {
+        String targetName = joinPoint.getTarget().getClass().getName();
+        String methodName = joinPoint.getSignature().getName();
+        Object[] arguments = joinPoint.getArgs();
+        Class targetClass = Class.forName(targetName);
+        Method[] methods = targetClass.getMethods();
+        String description = "";
+        for (Method method : methods) {
+            if (method.getName().equals(methodName)) {
+                Class[] clazzs = method.getParameterTypes();
+                if (clazzs.length == arguments.length) {
+                    description = method.getAnnotation(SystemControllerLog.class).description();
+                    break;
+                }
+            }
+        }
+        return description;
+    }
+}
--- src/main/java/com/flybeifeng/controller/StudentController.java
+++ src/main/java/com/flybeifeng/controller/StudentController.java
@@ -1,5 +1,7 @@
 package com.flybeifeng.controller;
 
+import com.flybeifeng.annotation.SystemControllerLog;
+import com.flybeifeng.annotation.SystemServiceLog;
 import com.flybeifeng.service.StudentService;
 import com.flybeifeng.bean.Student;
 import org.springframework.stereotype.Controller;
@@ -18,6 +20,7 @@ public class StudentController {
 
 
     @RequestMapping(value = "/getAllStudent", method = RequestMethod.GET)
+    @SystemControllerLog(description = "查询所有用户")
     public ModelAndView getAllStudent() {
         ModelAndView mav = new ModelAndView();
         mav.setViewName("/WEB-INF/views/studentDisplay.jsp");
@@ -26,18 +29,18 @@ public class StudentController {
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    @SystemControllerLog(description = "通过id查询用户")
+    @SystemServiceLog(description = "通过id查询用户")
     public @ResponseBody Student getStudentById(@PathVariable int id) {
-        System.out.println("id = " + id);
         return studentService.getStudentById(id);
     }
 
     @RequestMapping(value = "/put", method = RequestMethod.GET)
-    public ModelAndView addStudentById(Model model,
-                                       @RequestParam(value = "id") int id,
+    @SystemControllerLog(description = "新增用户")
+    public ModelAndView addStudentById(@RequestParam(value = "id") int id,
                                        @RequestParam(value = "name", required = false, defaultValue = "0") String name,
                                        @RequestParam(value = "age") int age,
                                        @RequestParam(value = "sex") boolean sex) {
-        System.out.println("id = " + id);
         studentService.addStudentById(id, name, age, sex);
 
         ModelAndView modelAndView = new ModelAndView();
@@ -47,8 +50,9 @@ public class StudentController {
     }
 
     @RequestMapping(value = "/delete", method = RequestMethod.GET)
+    @SystemControllerLog(description = "删除用户")
     public ModelAndView deleteStudentById(@RequestParam(value = "id") int id) {
-        System.out.println("id = " + id);
+
         studentService.deleteStudentById(id);
 
         ModelAndView modelAndView = new ModelAndView();
@@ -58,9 +62,9 @@ public class StudentController {
     }
 
     @RequestMapping(value = "/update", method = RequestMethod.GET)
+    @SystemControllerLog(description = "修改用户年龄")
     public ModelAndView updateStudentById(@RequestParam(value = "id") int id,
                                           @RequestParam(value = "age") int age) {
-        System.out.println("id = " + id);
         studentService.updateStudentById(id, age);
 
         ModelAndView modelAndView = new ModelAndView();
